b7084201b02f978b519dd875e0fc06d5f384b012
clicube <clicube@gmail.com>
2013-01-19 17:53:04 +0900

    add ConditionVariable

---
+++ conditionvariable.rb
@@ -0,0 +1,80 @@
+require_relative 'semaphore'
+
+module MultiProcessing
+  class ConditionVariable
+
+    def initialize name=nil
+      @waiting = Semaphore.new 0
+      @signal = Semaphore.new 0
+    end
+
+    def signal
+      begin
+        @waiting.trywait
+        @signal.post
+        return true
+      rescue => e
+        return nil
+      end
+    end
+
+    def broadcast
+      while(signal)
+      end
+    end
+
+    def wait mutex
+      @waiting.post
+      mutex.unlock
+      @signal.wait
+      mutex.lock
+      self
+    end
+
+  end
+end
+
+if __FILE__ == $0
+
+  require_relative 'mutex'
+ 
+  m = MultiProcessing::Mutex.new
+  cond = MultiProcessing::ConditionVariable.new
+  fork do
+    m.synchronize do
+      sleep 1
+      puts "waiting p1"
+      cond.wait(m)
+      puts "restarted p1"
+      sleep 1
+      puts "end p1"
+    end 
+  end 
+  fork do
+    m.synchronize do
+      sleep 1
+      puts "waiting p2"
+      cond.wait(m)
+      puts "restarted p2"
+      sleep 1
+      puts "end p2"
+    end 
+  end 
+  fork do
+    m.synchronize do
+      sleep 1
+      puts "waiting p3"
+      cond.wait(m)
+      puts "restarted p3"
+      sleep 1
+      puts "end p3"
+    end 
+  end 
+  sleep 5
+  puts "cond signaling"
+  cond.signal
+  sleep 3
+  puts "cond broadcasting"
+  cond.broadcast
+  Process.waitall
+end
