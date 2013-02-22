ee879c8bafb6dd4c124071ac41a6f63d9b1e2258
clicube <clicube@gmail.com>
2013-02-23 00:37:37 +0900

    rewrote spec of Mutex

--- spec/multiprocessing/mutex_spec.rb
+++ spec/multiprocessing/mutex_spec.rb
@@ -7,108 +7,260 @@ describe MultiProcessing::Mutex do
     @mutex = MultiProcessing::Mutex.new
   end
 
-  it "#lock returns itself" do
-    @mutex.lock.should === @mutex
-  end
+  describe "#lock" do
 
-  it "#unlock returns itself if locked" do
-    @mutex.lock
-    @mutex.unlock.should === @mutex
-  end
-
-  it "#unlock raises ProcessError if not locked" do
-    proc{ @mutex.unlock }.should raise_error MultiProcessing::ProcessError
-  end
-
-  it "#locked? returns false if not locked" do
-    @mutex.locked?.should be_false
-  end
-
-  it "#locked? returns true if locked" do
-    @mutex.lock
-    @mutex.locked?.should be_true
-  end
-
-  it "#lock, #unlock can locks/unlocks other process" do
-    pid = fork do
-      sleep 0.1
-      @mutex.lock
-      @mutex.unlock
+    it "returns itself" do
+      @mutex.lock.should === @mutex
     end
-    th = Process.detach(pid)
-    @mutex.lock
-    sleep 0.2
-    th.join(0).should be_nil
-    @mutex.unlock
-    proc{ timeout(1){ th.join } }.should_not raise_error Timeout::Error
-    th.kill
+
+    context "when locked in same thread" do
+
+      before do
+        @mutex.lock
+      end
+
+      it "raises ProcessError" do
+        proc{ @mutex.lock }.should raise_error MultiProcessing::ProcessError
+      end
+
+      after do
+        @mutex.unlock
+      end
+
+    end
+
+    context "when locked in another thread but same process" do
+
+      before do
+        Thread.new do
+          @mutex.lock
+          sleep 0.3
+          @mutex.unlock
+        end
+        sleep 0.1
+      end
+
+      it "blocks" do
+        proc{ timeout(0.1){ @mutex.lock } }.should raise_error Timeout::Error
+      end
+
+    end
+
+    context "when locked in another process" do
+
+      before do
+        fork do
+          @mutex.lock
+          sleep 0.2
+          @mutex.unlock
+        end
+        sleep 0.1
+      end
+
+      it "blocks" do
+        proc{ timeout(0.1){ @mutex.lock } }.should raise_error Timeout::Error
+      end
+
+    end
+
   end
 
-  it "#synchronize can synchronize with other process" do
-    pid = fork do
+  describe "#unlock" do
+
+    context "when locked in same thread" do
+
+      before do
+        @mutex.lock
+      end
+
+      it "return itself" do
+        @mutex.unlock.should === @mutex
+      end
+
+      context "when another thread in same process is blocked" do
+
+        before do
+          @thread = Thread.new do
+            @mutex.lock
+            @mutex.unlock
+          end
+          sleep 0.1
+        end
+
+        it "makes the other blocked thread restart" do
+          @mutex.unlock
+          proc{ timeout(0.1){ @thread.join } }.should_not raise_error Timeout::Error
+        end
+
+      end
+
+      context "when another thread in another process is blocked" do
+
+        before do
+          @pid = fork do
+            @mutex.lock
+            @mutex.unlock
+          end
+          sleep 0.1
+        end
+
+        it "makes the other blocked thread restart" do
+          @mutex.unlock
+          proc{ timeout(0.1){ @thread.join } }.should_not raise_error Timeout::Error
+        end
+
+        after do
+          begin
+            Process.kill :TERM, @pid
+          ensure Errno::ESRCH
+          end
+        end
+
+      end
+
+    end
+
+
+    context "when locked in another thread but same process" do
+
+      before do
+        Thread.new do
+          @mutex.lock
+          sleep 0.2
+          @mutex.unlock
+        end
+        sleep 0.1
+      end
+
+      it "raise ProcessError" do
+        proc{ @mutex.unlock }.should raise_error MultiProcessing::ProcessError
+      end
+
+      it "is still locked" do
+        begin
+          @mutex.unlock
+        rescue MultiProcessing::ProcessError
+        end
+        @mutex.should be_locked
+      end
+
+    end
+
+    context "when locked in another process" do
+
+      before do
+        fork do
+          @mutex.lock
+          sleep 0.2
+          @mutex.unlock
+        end
+        sleep 0.1
+      end
+
+      it "raises ProcessError" do
+        proc{ @mutex.unlock }.should raise_error MultiProcessing::ProcessError
+      end
+
+      it "is still locked" do
+        begin
+          @mutex.unlock
+        rescue MultiProcessing::ProcessError
+        end
+        @mutex.should be_locked
+      end
+
+    end
+
+    context "when not locked" do
+      it "raises ProcessError" do
+        proc{ @mutex.unlock }.should raise_error MultiProcessing::ProcessError
+      end
+    end
+
+  end
+
+
+  describe "#locked?" do
+
+    context "when not locked" do
+      it "returns false" do
+        @mutex.locked?.should be_false
+      end
+    end
+
+    context "when locked" do
+      it "returns true" do
+        fork{ @mutex.lock }
+        sleep 0.1
+        @mutex.locked?.should be_true
+      end
+    end
+
+  end
+
+  describe "#try_lock" do
+
+    context "when not locked" do
+
+      it "returns true" do
+        @mutex.try_lock.should be_true
+      end
+
+      it "gets locked" do
+        fork{ @mutex.try_lock }
+        sleep 0.1
+        @mutex.should be_locked
+      end
+
+    end
+
+    context "when locked" do
+
+      before do
+        fork do
+          @mutex.lock
+          sleep 0.2
+        end
+        sleep 0.1
+      end
+
+      it "returns false" do
+        @mutex.try_lock.should be_false
+      end
+
+      it "is still locked" do
+        @mutex.try_lock
+        @mutex.should be_locked
+      end
+
+    end
+  end
+
+  describe "#synchronoze" do
+
+    it "is locked in processing block" do
+      @mutex.synchronize do
+        @mutex.should be_locked
+      end
+    end
+
+    it "is locked in processing block in other process" do
+      fork do
+        @mutex.synchronize do
+          sleep 0.2
+        end
+      end
       sleep 0.1
+      @mutex.should be_locked
+    end
+
+    it "is unlocked after block" do
       @mutex.synchronize do
         :nop
       end
+      @mutex.should_not be_locked
     end
-    th = Process.detach(pid)
-    @mutex.synchronize do
-      sleep 0.2
-      th.join(0).should be_nil
-    end
-    proc{ timeout(1){ th.join } }.should_not raise_error Timeout::Error
   end
-
-  it "#lock cannot lock twice" do
-    @mutex.lock
-    proc{
-      @mutex.lock
-    }.should raise_error MultiProcessing::ProcessError
-  end
-
-  it "#unlock raises ProcessError and does not unlock when unlocking by other processes" do
-    pid = fork do
-      @mutex.lock
-      sleep 0.2
-      @mutex.unlock
-    end
-    sleep 0.1
-    th = Process.detach(pid)
-    proc{ @mutex.unlock }.should raise_error MultiProcessing::ProcessError
-    th.value.success?.should be_true
-  end
-
-  it "#try_lock returns true if succeed, and lock correctly" do
-    @mutex.try_lock.should be_true
-    @mutex.locked?.should be_true
-    @mutex.unlock.should === @mutex
-  end
-
-  it "#try_lock returns false if failed" do
-    pid = fork do
-      @mutex.lock
-      sleep 0.2
-    end
-    sleep 0.1
-    @mutex.try_lock.should be_false
-  end
-
-  it "s sleep method unlocks and sleeps and re-locks itself" do
-    pid = fork do
-      @mutex.lock
-      @mutex.sleep(0.2)
-      sleep 0.2
-      @mutex.unlock
-    end
-    th = Process.detach(pid)
-    sleep 0.1
-    @mutex.try_lock.should be_true
-    @mutex.unlock
-    sleep 0.2
-    @mutex.try_lock.should be_false
-    timeout(1){ th.value }.success?.should be_true
-  end
-
 
 end
 
