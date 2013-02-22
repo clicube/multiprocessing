0106bf26e8223ec1d8f113e7fdac1a13650e9dee
clicube <clicube@gmail.com>
2013-02-23 04:17:28 +0900

    added document of Mutex

--- lib/multiprocessing/mutex.rb
+++ lib/multiprocessing/mutex.rb
@@ -3,15 +3,40 @@ require File.expand_path(File.dirname(__FILE__) + '/processerror')
 
 module MultiProcessing
 
+  ##
+  #
+  # Process version of Mutex.
+  # This can be used like ::Mutex in Ruby standard library.
+  #
+  # Do not fork in #synchronize block or before #unlock.
+  # Forking and forked process run in parallel.
+  #
+  # Note that Mutex uses 1 pipe.
+  #
+  # @example
+  #   mutex = MultiProcessing::Mutex.new
+  #   fork
+  #   mutex.synchronize do
+  #     # critical section
+  #     puts Process.pid
+  #     sleep 1
+  #   end
+  # 
   class Mutex
 
     def initialize
       @pout,@pin = IO.pipe
       @pin.syswrite 1
-      #@pin.write 1
-      #@pin.flush
     end
 
+    ##
+    #
+    # Attempts to grab the lock and waits if it isn't available.
+    # Raises ProcessError if mutex was locked by the current thread.
+    #
+    # @return [Mutex] itself
+    # @raise [ProcessError]
+    #
     def lock
       unless @locking_pid == ::Process.pid && @locking_thread == Thread.current
         @pout.readpartial 1
@@ -23,18 +48,29 @@ module MultiProcessing
       self
     end
 
+    ##
+    #
+    # Returns true if this lock is currently held by some thread.
+    #
+    # @return [Boolean]
+    #
     def locked?
       begin
         @pout.read_nonblock 1
         @pin.syswrite 1
-        #@pin.write 1
-        #@pin.flush
         return false
       rescue Errno::EAGAIN => e
         return true
       end
     end
 
+    ##
+    #
+    # Attempts to obtain the lock and returns immediately.
+    # Returns true if the lock was granted.
+    #
+    # @return [Boolean]
+    #
     def try_lock
       begin
         @pout.read_nonblock 1
@@ -46,6 +82,14 @@ module MultiProcessing
       end
     end
 
+    ##
+    #
+    # Releases the lock.
+    # Raises ProcessError if mutex wasn't locked by the current thread.
+    #
+    # @return [Boolean]
+    # @raise [ProcessError]
+    #
     def unlock
       raise ProcessError.new("Attempt to unlock a mutex which is not locked") if !locked?
       if @locking_pid == ::Process.pid && @locking_thread == Thread.current
@@ -56,11 +100,16 @@ module MultiProcessing
         @locking_thread = nil
         return self
       else
-        #return nil
         raise ProcessError.new("mutex was tried unlocking in process/thread which didn't lock this mutex #{@locking_pid} #{::Process.pid}")
       end
     end
 
+    ##
+    #
+    # Obtains a lock, runs the block, and releases the lock when the block completes.
+    #
+    # @return [Object] returned value of block
+    #
     def synchronize
       lock
       begin
@@ -71,6 +120,14 @@ module MultiProcessing
       return ret
     end
 
+    ##
+    #
+    # Releases the lock and sleeps timeout seconds if it is given and non-nil or forever.
+    # Raises ProcessError if mutex wasn't locked by the current thread.
+    #
+    # @param [Numeric,nil] timeout
+    # @raise [ProcessError]
+    #
     def sleep timeout=nil
       unlock
       timeout ? Kernel.sleep(timeout) : Kernel.sleep
