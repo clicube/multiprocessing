ffd7474fd82ee2cc28fd9183245e791384756f33
clicube <clicube@gmail.com>
2013-02-24 18:11:13 +0900

    added documentation for ConditionVariable

--- lib/multiprocessing/conditionvariable.rb
+++ lib/multiprocessing/conditionvariable.rb
@@ -2,6 +2,31 @@ require File.expand_path(File.dirname(__FILE__) + '/processerror')
 require File.expand_path(File.dirname(__FILE__) + '/mutex')
 
 module MultiProcessing
+
+  ##
+  #
+  # Process version of ConditionVariable
+  # This can be used like ::ConditionVariable in Ruby standard library.
+  #
+  # Note that ConditionVariable uses 2 pipes.
+  #
+  # @example
+  #   m = MultiProcessing::Mutex.new
+  #   cond = MultiProcessing::ConditionVariable.new
+  #   3.times do
+  #     fork do
+  #       m.synchronize do
+  #         puts "waiting pid:#{Process.pid}"
+  #         cond.wait(m)
+  #         puts "restarted pid:#{Process.pid}"
+  #       end
+  #     end
+  #   end
+  #   sleep 0.1 # => 3 processes get waiting a signal
+  #   cond.signal # => One process restarts
+  #   cond.broadcast # => Remaining 2 process restart
+  #   Process.waitall
+  #
   class ConditionVariable
 
     def initialize
@@ -9,6 +34,12 @@ module MultiProcessing
       @signal_pout,@signal_pin = IO.pipe
     end
 
+    ##
+    #
+    # Wakes up all threads waiting for this lock.
+    #
+    # @return [Fixnum] Number of threads waked up
+    #
     def broadcast
       n = 0
       while(signal)
@@ -17,6 +48,12 @@ module MultiProcessing
       return n
     end
 
+    ##
+    #
+    # Wakes up one of threads waiting for this lock.
+    #
+    # @return [Boolean] Returns true if wakes up. Returns false if no threads were waiting.
+    #
     def signal
       begin
         @waiting_pout.read_nonblock 1
@@ -27,6 +64,17 @@ module MultiProcessing
       end
     end
 
+    ##
+    #
+    # Releases the lock held in mutex and waits, reacquires the lock on wakeup.
+    #
+    # @param [Mutex] mutex An instance of MultiProcessing::Mutex. It must be locked.
+    # @return [ConditionVariable] itself
+    # @note Do not pass an instance of ::Mutex. Pass an instance of MultiProcessing::Mutex.
+    #
+    # @raise [TypeError]
+    # @raise [ArgumentError]
+    #
     def wait(mutex)
       raise TypeError.new("mutex must be instance of MultiProcessing::Mutex") if mutex.class != MultiProcessing::Mutex
       raise ArgumentError.new("mutex must be locked") unless mutex.locked?
@@ -38,48 +86,5 @@ module MultiProcessing
     end
 
   end
-end
-
-if __FILE__ == $0
-
-  m = MultiProcessing::Mutex.new
-  cond = MultiProcessing::ConditionVariable.new
-  fork do
-    m.synchronize do
-      sleep 1
-      puts "waiting p1"
-      cond.wait(m)
-      puts "restarted p1"
-      sleep 1
-      puts "end p1"
-    end
-  end
-  fork do
-    m.synchronize do
-      sleep 1
-      puts "waiting p2"
-      cond.wait(m)
-      puts "restarted p2"
-      sleep 1
-      puts "end p2"
-    end
-  end
-  fork do
-    m.synchronize do
-      sleep 1
-      puts "waiting p3"
-      cond.wait(m)
-      puts "restarted p3"
-      sleep 1
-      puts "end p3"
-    end
-  end
-  sleep 5
-  puts "cond signaling"
-  cond.signal
-  sleep 3
-  puts "cond broadcasting"
-  cond.broadcast
-  Process.waitall
 end
 
