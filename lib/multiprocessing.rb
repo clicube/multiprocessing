6ede8fef276b0e22de7667219d81f2c9e620682c
clicube <clicube@gmail.com>
2013-02-27 19:53:01 +0900

    added documentation for MultiProcessing.try_handle_interrupt

--- lib/multiprocessing.rb
+++ lib/multiprocessing.rb
@@ -7,6 +7,14 @@ require File.expand_path(File.dirname(__FILE__) + '/multiprocessing/externalobje
 
 module MultiProcessing
 
+  ##
+  #
+  # If Thread has handle_interrupt (Ruby 2.0.0 or later), call it with given arguments and block.
+  # If not, simply yield passed block
+  #
+  # @param [Array<Object>] args arguments to give Thread.handle_interrupt
+  # @return [Object] returned value of the block
+  #
   def try_handle_interrupt *args
     if Thread.respond_to?(:handle_interrupt)
       Thread.handle_interrupt(*args){ yield }
